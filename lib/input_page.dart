8e872ea4a4f36311a0ae8ff8c9ce4636e50fe823
zoyasarfraz <zoyas26870@gmqail.com>
2020-11-26 10:58:34 +0500

    he1

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -20,7 +20,7 @@ class _InputPageState extends State<InputPage> {
   Gender selectedGender;
   int height = 120;
   int weight = 50;
-  int age = 20;
+  int age = 15;
 
   @override
   Widget build(BuildContext context) {
